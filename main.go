09bd97f7d4d202cd40b69ad8b8bedf4deea2df69
G.S.D <51993109+GhostSwordsman@users.noreply.github.com>
2021-06-15 01:09:15 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,35 @@
+// 生成DGA域名
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"strings"
+	"time"
+)
+
+var ch = make(chan string)
+
+
+func createDomain(year, month, day, count int, secret string, tld string) {
+	if !strings.HasPrefix(tld, ".") {
+		tld = "." + tld
+	}
+
+	md5Hash := fmt.Sprintf("%x", md5.Sum([]byte(fmt.Sprintf("%v%v%v%v%v", year, month, day, count,secret))))
+	domain := fmt.Sprintf("%v%v", md5Hash[12:23], tld)
+	ch <- domain
+}
+
+
+func main() {
+	t := time.Now()
+	y, m, d := t.Year(), int(t.Month()), t.Day()
+
+	for i:=1; i<10; i++ {
+		go createDomain(y, m, d, i,"Chelsea", "com")
+
+		domain := <-ch
+		fmt.Println(domain)
+	}
+}
